ebce10099db7e4d1dbf941f0fb67390d1542eac6
Your Name <you@example.com>
2023-03-08 22:04:52 +0700

    fixing bug

--- main.go
+++ main.go
@@ -14,8 +14,7 @@ func main() {
 	for j := 0; j <= 10; j++ {
 		switch j {
 		case 5:
-			//Cara pengerjaan hanya apa yang ada di pikiran saya.
-			str := "C A Ш A P B O"
+			str := "САШАРВО"
 			bytePosition := 0
 
 			for i, char := range str {
@@ -26,19 +25,6 @@ func main() {
 					fmt.Printf("character U+%04X '%s'  starts at byte position %d\n", char, string(char), bytePosition)
 				}
 			}
-
-			//Notes problem
-			//output yang dihasilkan pada saat nilai J == 5 adalah:
-			//character U+0043 'C'  starts at byte position 0
-			//character U+0041 'A'  starts at byte position 2
-			//character U+0428 'Ш'  starts at byte position 4
-			//character U+0020 ' '  starts at byte position 6
-			//character U+0020 ' '  starts at byte position 8
-			//character U+0020 ' '  starts at byte position 10
-			//character U+0020 ' '  starts at byte position 12
-
-			//Tidak tahu kenapa setelah char Ш (Cyrillic) selalu terjadi kesalahan output pada char yang tersisa,
-			//tapi saya sudah berusahan sebisanya, mungkin jika ada solusi bisa dibahas di sesi selanjutnya
 
 		default:
 			println("Nilai j adalah", j)
